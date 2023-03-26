bfd72bbe8b3a3e258fe590891c393f3a128d570b
okailahmed <ahmedokeelbuss@gmail.com>
2023-03-26 21:54:40 +0200

    this commite include allocate memory space and assigne value using double pointer

--- Pointer/pointer.c
+++ Pointer/pointer.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+#include <malloc.h>
 
+void allocateMemory(int **ptr)
+{
 
+    *ptr = (int *)malloc(sizeof(int)); // allocate some memory
+}
 
-
-int main()
+void simpleoperationdoublePointer()
 {
     int num = 123;
-    
+
     int *singlePointer = NULL;
 
     int **doublePointer = NULL;
@@ -15,26 +19,39 @@ int main()
 
     doublePointer = &singlePointer;
 
-    //to print values with different ways 
+    // to print values with different ways
 
-    printf("\n value of Num is : %d", num );
-    printf("\n value of Num using singlPointer  is : %d", *singlePointer );
+    printf("\n value of Num is : %d", num);
+    printf("\n value of Num using singlPointer  is : %d", *singlePointer);
     printf("\n value of Num using doublepointer is  : %d", **doublePointer);
 
-    //to print address of different values 
+    // to print address of different values
 
-    printf("\n adddress of Num is : %p", &num );
-    printf("\n adddress of Num using singlPointer  is : %p", singlePointer );
+    printf("\n adddress of Num is : %p", &num);
+    printf("\n adddress of Num using singlPointer  is : %p", singlePointer);
     printf("\n adddress of Num using doublepointer is  : %p", *doublePointer);
 
-    //to print value of pointers 
+    // to print value of pointers
 
-    printf("\n value  of  singlPointer  is : %d", *singlePointer );
+    printf("\n value  of  singlPointer  is : %d", *singlePointer);
     printf("\n value  of  doublepointer is  : %d", **doublePointer);
 
-    //to print address of pointers
-    printf("\n address  of  singlPointer  is : %p", &singlePointer );
+    // to print address of pointers
+    printf("\n address  of  singlPointer  is : %p", &singlePointer);
     printf("\n address  of  doublepointer is  : %p", &doublePointer);
+}
 
+int main()
+{
+    int *ptr = NULL;
+    
+    allocateMemory(&ptr);
+
+    *ptr = 20;
+
+    printf("%d \n ", *ptr);
+
+
+    free(ptr);
     return 0;
 }
