c3953bc4328afc827b4c42035c4b07373d52ba85
okailahmed <ahmedokeelbuss@gmail.com>
2023-03-27 00:12:54 +0200

    this commit include convert decimal to binary

--- Bit_manipulation/Binary_number_bits.c
+++ Bit_manipulation/Binary_number_bits.c
@@ -1,39 +1,66 @@
 #include <stdio.h>
 #include <math.h>
 
-
 int convertBinaryToDecimal(long long n);
 
-
+long long convertDecimalToBinary(int n);
 
 int main()
 {
-    long long n;
+    long long Bin_num, binary_result;
 
-    int result =0;
+    int decimal_result = 0, Dec_num;
 
     printf("Enter a binary number : ");
 
-    scanf("%lld", &n);
+    scanf("%lld", &Bin_num);
 
-    result = convertBinaryToDecimal(n);
+    decimal_result = convertBinaryToDecimal(Bin_num);
 
-    printf("%lld in binary =  %d in decimal ", n,result);
+    printf("%lld in binary =  %d in decimal ", Bin_num, decimal_result);
+    101printf("\n");
+    // this code to convert binary to decimal
+    printf("Enter a Decimal number : ");
 
-   return 0;
+    scanf("%d", &Dec_num);
+
+    binary_result = convertDecimalToBinary(Dec_num);
+
+
+    printf("%d in Decimal =  %lld in Binary ", Dec_num, binary_result);
+
+    return 0;
 }
-
 
 int convertBinaryToDecimal(long long n)
 {
 
-    int decimalNumber =0, i =0 , reminder =0;
+    int decimalNumber = 0, i = 0, reminder = 0;
+
+    while (n != 0)
+    {
+        reminder = n % 10;
+        n = n / 10;
+        decimalNumber += reminder * pow(2, i);
+        ++i;
+    }
+}
+
+long long convertDecimalToBinary(int n)
+{
+    long long binaryNumber =0;
+    int reminder , i=1;
+
 
     while(n != 0)
     {
-        reminder = n% 10;
-        n = n /10;
-        decimalNumber += reminder*pow(2,i);
-        ++i;
+        reminder = n%2;
+
+        n = n/2;
+
+        binaryNumber += reminder * i;
+        i = i * 10;
     }
+
+    return binaryNumber;
 }
